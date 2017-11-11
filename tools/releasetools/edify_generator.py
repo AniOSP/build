d217ad4f16d65048fc8eea7d4403e310bcfac9a6
jhenrique09 <jhenrique09.mcz@hotmail.com>
2017-11-11 17:24:03 -0200

    Show ROM info while flashing
    Change-Id: I0769a74d70621536d3794bcdc3e70847945f00b5
    Signed-off-by: Daryl Ronningen <relms@relms.dev>

--- tools/releasetools/edify_generator.py
+++ tools/releasetools/edify_generator.py
@@ -264,6 +264,18 @@ class EdifyGenerator(object):
     """Log a message to the screen (if the logs are visible)."""
     self.script.append('ui_print("%s");' % (message,))
 
+  def PrintAniospBanner(self, android_version, build_id, build_date, security_patch, device):
+    self.Print("----------------------------------------------")
+    self.Print("              AniOSP")
+    self.Print("              by @ech0devv & @relms")
+    self.Print("----------------------------------------------")
+    self.Print(" Android version: %s"%(android_version))
+    self.Print(" Build id: %s"%(build_id))
+    self.Print(" Build date: %s"%(build_date))
+    self.Print(" Security patch: %s"%(security_patch))
+    self.Print(" Device: %s"%(device))
+    self.Print("----------------------------------------------")
+
   def TunePartition(self, partition, *options):
     fstab = self.fstab
     if fstab:
